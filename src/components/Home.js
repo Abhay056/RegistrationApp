703c13bd98661cd14076665b70ac9ff4836f3bc2
Abhay <77850911+Abhay056@users.noreply.github.com>
2025-05-12 14:34:53 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <h1 className="head">
+          Welcome <br/> <br/> <br/>
+          Click Getting Started to start Registration
+        </h1>
+    )
+}
